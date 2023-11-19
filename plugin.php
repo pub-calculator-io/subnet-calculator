c006c12de367f86e186f4f7665bb452ffe0e9e5e
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:26:50 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Subnet Calculator by Calculator.iO
+Plugin Name: Subnet Calculator by www.calculator.io
 Plugin URI: https://www.calculator.io/subnet-calculator/
 Description: This IP subnet calculator makes subnetting easy, providing full subnet information like IP ranges, subnet masks and more from just basic network information.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Subnet Calcula
 
 function display_ci_subnet_calculator(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/subnet-calculator/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Subnet Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_subnet_calculator_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Subnet Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_subnet_calculator_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_subnet_calculator', 'display_ci_subnet_calculator' );
